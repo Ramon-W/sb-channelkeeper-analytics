daa8ff9e40fa3325366e330769e6b394ceaaf560
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-15 21:04:39 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,22 @@
+from flask import Flask, redirect, Markup, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+from flask import render_template
+from bson.objectid import ObjectId
+from mongosanitizer.sanitizer import sanitize #documentation says used to sanitize user input quieries, but no quieries here are performed with user input.
 
+import pprint
+import os
+import sys
+import pymongo
+from datetime import datetime, timedelta
+from pytz import timezone
+import pytz
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_layout():
+    return render_template('layout.html')
