34f570aa29086f625a92e921371e4fba8dd1131e
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-20 18:10:53 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,7 +12,7 @@ import pandas as pd
 app = Flask(__name__)
 
 #put on render page with map
-gp = gspread.service_account(jsonify(type = os.environ['type'], project_id = os.environ['project_id'], private_key_id = os.environ['private_key_id'], private_key = os.environ['private_key'], client_email = os.environ['client_email'], client_id = os.environ['client_id'], auth_uri = os.environ['auth_uri'], token_uri = os.environ['token_uri'], auth_provider_x509_cert_url = os.environ['auth_provider_x509_cert_url'], client_x509_cert_url = os.environ['client_x509_cert_url']))
+gp = gspread.service_account(type = os.environ['type'], project_id = os.environ['project_id'], private_key_id = os.environ['private_key_id'], private_key = os.environ['private_key'], client_email = os.environ['client_email'], client_id = os.environ['client_id'], auth_uri = os.environ['auth_uri'], token_uri = os.environ['token_uri'], auth_provider_x509_cert_url = os.environ['auth_provider_x509_cert_url'], client_x509_cert_url = os.environ['client_x509_cert_url'])
 gsheet = gp.open('Watershed Brigade Clean-up and Point Request Form (Responses)')
 wsheet = gsheet.worksheet('Form Responses 1')
 
