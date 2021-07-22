18db7f6c3927301e787f394cfc6c1c3f47713cce
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-22 12:08:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,23 +11,26 @@ import pandas as pd
 
 app = Flask(__name__)
 
-#put on render page with map
-credentials = {
-    "type": "service_account",
-    "project_id": os.environ['project_id'],
-    "private_key_id": os.environ['private_key_id'],
-    "private_key": os.environ['private_key'].replace('\\n', '\n'),
-    "client_email": os.environ['client_email'],
-    "client_id": os.environ['client_id'],
-    "auth_uri": os.environ['auth_uri'],
-    "token_uri": os.environ['token_uri'],
-    "auth_provider_x509_cert_url": os.environ['auth_provider_x509_cert_url'],
-    "client_x509_cert_url": os.environ['client_x509_cert_url']
-}
-gp = gspread.service_account_from_dict(credentials)
-gsheet = gp.open('Copy of Watershed Brigade')
-wsheet = gsheet.worksheet('2021 WB Tracking')
+try:
+    credentials = {
+        "type": "service_account",
+        "project_id": os.environ['project_id'],
+        "private_key_id": os.environ['private_key_id'],
+        "private_key": os.environ['private_key'].replace('\\n', '\n'),
+        "client_email": os.environ['client_email'],
+        "client_id": os.environ['client_id'],
+        "auth_uri": os.environ['auth_uri'],
+        "token_uri": os.environ['token_uri'],
+        "auth_provider_x509_cert_url": os.environ['auth_provider_x509_cert_url'],
+        "client_x509_cert_url": os.environ['client_x509_cert_url']
+    }
+    gp = gspread.service_account_from_dict(credentials)
+    gsheet = gp.open('Copy of Watershed Brigade')
+    wsheet = gsheet.worksheet('2021 Wba Tracking')
+except Exception as inst:
+    print(inst)
+    return render_template('error.html')
 
-@app.route('/')
-def render_layout():
-    return render_template('layout.html', data = wsheet.get_all_values())
+@app.route('/') #change start route later?
+def render_map():
+    return render_template('map.html', data = wsheet.get_all_values())
