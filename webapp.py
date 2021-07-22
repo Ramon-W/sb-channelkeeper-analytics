b6bd8cdaf2a6d0360f3320d2c2f1d5eec30a34c1
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-22 12:39:53 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,24 +8,27 @@ from datetime import datetime, timedelta
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+from datetime import datetime, timedelta
+import pytz
 
 app = Flask(__name__)
 
 credentials = {
-    "type": "service_account",
-    "project_id": os.environ['project_id'],
-    "private_key_id": os.environ['private_key_id'],
-    "private_key": os.environ['private_key'].replace('\\n', '\n'),
-    "client_email": os.environ['client_email'],
-    "client_id": os.environ['client_id'],
-    "auth_uri": os.environ['auth_uri'],
-    "token_uri": os.environ['token_uri'],
-    "auth_provider_x509_cert_url": os.environ['auth_provider_x509_cert_url'],
-    "client_x509_cert_url": os.environ['client_x509_cert_url']
+    'type': 'service_account',
+    'project_id': os.environ['project_id'],
+    'private_key_id': os.environ['private_key_id'],
+    'private_key': os.environ['private_key'].replace('\\n', '\n'),
+    'client_email': os.environ['client_email'],
+    'client_id': os.environ['client_id'],
+    'auth_uri': os.environ['auth_uri'],
+    'token_uri': os.environ['token_uri'],
+    'auth_provider_x509_cert_url': os.environ['auth_provider_x509_cert_url'],
+    'client_x509_cert_url': os.environ['client_x509_cert_url']
 }
+utc_year = int(datetime.now().strftime('%Y'))
 gp = gspread.service_account_from_dict(credentials)
 gsheet = gp.open('Copy of Watershed Brigade')
-wsheet = gsheet.worksheet('2021 WB Tracking')
+wsheet = gsheet.worksheet(utc_year + ' WB Tracking')
 
 @app.route('/') #change start route later?
 def render_map():
