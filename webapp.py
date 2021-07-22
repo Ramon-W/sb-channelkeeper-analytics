c69d16df35f09801e62bf815e3fb4a31e6b977cf
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-22 14:56:33 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -28,6 +28,17 @@ utc_year = datetime.now().strftime('%Y')
 gp = gspread.service_account_from_dict(credentials)
 gsheet = gp.open('Copy of Watershed Brigade')
 wsheet = gsheet.worksheet(utc_year + ' WB Tracking')
+data_new = wsheet.get_all_values()
+gsheet = gp.open('Watershed Brigade Information')
+wsheet = gsheet.worksheet('Sheet1')
+data_old = wsheet.get_all_values()
+data_intermediate = []
+for row in data_new:
+    if row[1] != '' and row[2].isnumeric() and row[3] != '' and row[4] != '' and row[5].isnumeric() and row[7].isnumeric() and row[8].isnumeric() and row[15].isnumeric():
+        data_intermediate.append(row)
+if data_intermediate != data_old:
+    gsheet.del_worksheet(wsheet)
+    gsheet.add_worksheet(title="Sheet1", rows="100", cols="20")
 
 @app.route('/') #change start route later?
 def render_map():
