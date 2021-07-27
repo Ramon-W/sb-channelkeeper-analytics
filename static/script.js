f7fbb69f9ff72f3769252a0a04e07e53517f4674
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-26 22:51:19 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#monthSelection").click(function() {
     if($("input.abc").not(":checked").length === 0)
     {
-      $("#map").html("Hello World");
+      $("#map").replaceWith("<p>Replaced</p>")
       var helloWorld = $('#map').html();
     }
     if($(#Jan).is(":checked"))
