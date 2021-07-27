80c69a63fc907d64178557455217a1baefbbf83b
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-27 14:01:02 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
       $("#map").replaceWith("<iframe id='map' src='https://www.geosheets.com/map/s:KJV92kAM/cleanups/embed' class='map'></iframe>")
     }
     else if ($('.Month:checked').length === 0) {
-      $("#map").replaceWith("<iframe id='map' src='https://www.geosheets.com/map/s:KJV92kAM/cleanups?filters=%7B%22a.%20Name%22%3A%22_all_%22%2C%22b.%20People%22%3A%22_all_%22%2C%22c.%20Date%22%3A%22_all_%22%2C%22d.%20Place(s)%22%3A%22_all_%22%2C%22e.%20Weight%20(lbs)%22%3A%22_all_%22%2C%22f.%20Bag(s)%22%3A%22_all_%22%2C%22g.%20Time%20(hrs)%22%3A%22_all_%22%7D' class='map'></iframe>")
+      alert("Please select at least one option");
     }
   });
   //$("#monthSelection").click(function() {
