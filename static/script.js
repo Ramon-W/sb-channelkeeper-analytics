eca66477ba0ee156ab3487b8a69ac3bec37db844
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-27 13:46:51 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,15 +1,23 @@
-$(document).ready(function(){ 
-  $("#monthSelection").click(function() {
-    if($("input.Month").not(":checked").length === 0)
-    {
+$(document).ready(function() { 
+  $(".Month").change(function() {
+    if ($('.Month:checked').length == $('.Month').length) {
       $("#map").replaceWith("<p>All Checked</p>")
     }
-    else if($("input.Month").not(":checked").length === 12)
-    {
+    else if ($('.Month:checked').length === 0) {
       $("#map").replaceWith("<p>All Not Checked</p>")
     }
-    if($("#Jan").is(":checked"))
-    {
-    }
-  }); 
+  });
+  //$("#monthSelection").click(function() {
+  //  if($("input.Month").not(":checked").length === 0)
+  //  {
+  //    $("#map").replaceWith("<p>All Checked</p>")
+  //  }
+  //  else if($("input.Month").not(":checked").length === 12)
+  //  {
+  //    $("#map").replaceWith("<p>All Not Checked</p>")
+  //  }
+  //  if($("#Jan").is(":checked"))
+  //  {
+  //  }
+  //}); 
 });
