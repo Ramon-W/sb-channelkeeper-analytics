e342149cdba935de8ca92f4168ad351864c9e714
Ramon Wang <ramonwang.sb@gmail.com>
2021-07-27 20:19:28 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -97,7 +97,7 @@ def get_data():
         counter -= 1
     if data_update != data_old:
         wsheet.update('A1:J' + str(len(data_update)), data_update)
-        wsheet.update('K1', '=GEO_MAP(A1:J254, "cleanups", "Location")')
+        wsheet.update('K1', '=GEO_MAP(A1:J' + str(len(data_update)) + ', "cleanups", "Location")')
     return data_new
 
 @app.route('/') #change start route later?
