4ac74e90f1fa700cbc1006e0112a6b912f2753aa
Ramon Wang <ramonwang.sb@gmail.com>
2021-08-10 19:28:40 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -266,18 +266,18 @@ def render_stats():
     counter = 1
     for key in chart_data:
         chart_data[key] = chart_data[key][:-1]
-	if counter < len(chart_data):
-	    chart += (']' +
-                  '},' +
-                  '{' +
-                  'type: "scatter",' +
-                  'name: "' + key + 'People",' +
-                  'showInLegend: true,' +
-                  'indexLabelFontSize: 16,' +
-                  'toolTipContent: "<span style=\"color:#4F81BC \"><b>{name}</b></span><br/><b> Time: </b> {x} hrs<br/><b> Weight of Trash </b></span> {y} lbs",' +
-                  'dataPoints: [')
-	chart += chart_data[key]
-	counter += 1
+        if counter < len(chart_data):
+            chart += (']' +
+                      '},' +
+                      '{' +
+                      'type: "scatter",' +
+                      'name: "' + key + 'People",' +
+                      'showInLegend: true,' +
+                      'indexLabelFontSize: 16,' +
+                      'toolTipContent: "<span style=\"color:#4F81BC \"><b>{name}</b></span><br/><b> Time: </b> {x} hrs<br/><b> Weight of Trash </b></span> {y} lbs",' +
+                      'dataPoints: [')
+    chart += chart_data[key]
+    counter += 1
     chart += ']}]'
     counter = 0
     months = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
