d2c4a9d63da3fe3c906375bc1a6f4ddf7bc169ba
Ramon Wang <ramonwang.sb@gmail.com>
2021-08-13 20:55:34 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() { 
-  $("#map_reports").hide();
+  $("#map-reports").hide();
   $("#reports").hide();
   $("#legend").hide();
   $(".Month").change(function() {
     if ($(".Month:checked").length == $(".Month").length) {
-      $("#map_cleanups").replaceWith("<iframe id='map_cleanups' src='https://www.geosheets.com/map/s:KJV92kAM/cleanups/embed' class='map'></iframe>")
+      $("#map-cleanups").replaceWith("<iframe id='map-cleanups' src='https://www.geosheets.com/map/s:KJV92kAM/cleanups/embed' class='map'></iframe>")
     }
     else if ($(".Month:checked").length === 0) {
-      $("#map_cleanups").replaceWith("<iframe id='map_cleanups' src='https://www.geosheets.com/map/s:KJV92kAM/cleanups?filters=%7B%22Month%22%3A%5B%22None%22%5D%2C%22a.%20Name%22%3A%22_all_%22%2C%22b.%20People%22%3A%22_all_%22%2C%22c.%20Date%22%3A%22_all_%22%2C%22d.%20Place(s)%22%3A%22_all_%22%2C%22e.%20Weight%20(lbs)%22%3A%22_all_%22%2C%22f.%20Bag(s)%22%3A%22_all_%22%2C%22g.%20Time%20(hrs)%22%3A%22_all_%22%7D' class='map'></iframe>")
+      $("#map-cleanups").replaceWith("<iframe id='map-cleanups' src='https://www.geosheets.com/map/s:KJV92kAM/cleanups?filters=%7B%22Month%22%3A%5B%22None%22%5D%2C%22a.%20Name%22%3A%22_all_%22%2C%22b.%20People%22%3A%22_all_%22%2C%22c.%20Date%22%3A%22_all_%22%2C%22d.%20Place(s)%22%3A%22_all_%22%2C%22e.%20Weight%20(lbs)%22%3A%22_all_%22%2C%22f.%20Bag(s)%22%3A%22_all_%22%2C%22g.%20Time%20(hrs)%22%3A%22_all_%22%7D' class='map'></iframe>")
     }
     else {
       var baseLink = "https://www.geosheets.com/map/s:KJV92kAM/cleanups?filters=%7B%22Month%22%3A%5B"
@@ -116,22 +116,22 @@ $(document).ready(function() {
         }
       }
       baseLink += "%5D%2C%22a.%20Name%22%3A%22_all_%22%2C%22b.%20People%22%3A%22_all_%22%2C%22c.%20Date%22%3A%22_all_%22%2C%22d.%20Place(s)%22%3A%22_all_%22%2C%22e.%20Weight%20(lbs)%22%3A%22_all_%22%2C%22f.%20Bag(s)%22%3A%22_all_%22%2C%22g.%20Time%20(hrs)%22%3A%22_all_%22%7D"
-      $("#map_cleanups").replaceWith("<iframe id='map_cleanups' src='" + baseLink + "' class='map'></iframe>")
+      $("#map-cleanups").replaceWith("<iframe id='map-cleanups' src='" + baseLink + "' class='map'></iframe>")
     }
   });
   $("#switcher-1").change(function() {
     if ($("#switcher-1").is(':checked')) {
       $("#cleanups").show();  // checked
       $("#reports").hide();
-      $("#map_reports").hide();
-      $("#map_cleanups").show();
+      $("#map-reports").hide();
+      $("#map-cleanups").show();
       $("#legend").hide();
     }
     else {
       $("#cleanups").hide();  // unchecked
       $("#reports").show();
-      $("#map_reports").show();
-      $("#map_cleanups").hide();
+      $("#map-reports").show();
+      $("#map-cleanups").hide();
       $("#legend").show();
     }
   });
