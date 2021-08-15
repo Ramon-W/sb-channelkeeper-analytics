d7e32bedad46884b882838c86cfee4b29145b43b
Ramon Wang <ramonwang.sb@gmail.com>
2021-08-15 14:47:27 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -131,7 +131,7 @@ def render_maps(): #renders the maps page.
         if row[4] == date_now.strftime('%m/%d/%Y'):
             reports += 1
             if row[3] == 'Not resolved':
-                resolve_locations = '<option>' + row[0] + '</option>'
+                resolve_locations += '<option>' + row[0] + '</option>'
     if reports >= 10:
         report_limit = '<p id="report-limit">The maximum number of reports have been reached, please try tomorrow.</p>'
         disable = 'disabled'
