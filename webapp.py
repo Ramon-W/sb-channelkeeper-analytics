b8eef654f85353aca606659df9c1ae999f99a4a6
Ramon Wang <ramonwang.sb@gmail.com>
2021-08-14 19:11:35 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -165,7 +165,7 @@ def render_maps_embed(): #same as render_maps() except this renders a page witho
     if len(data_report) > 40:
         location_question = '<label>Coordinates: ( <input name="x-location" class="form-control" placeholder="34.011761" maxlength="10" type="number" required> , <input name="y-location" class="form-control" placeholder="-119.777489" maxlength="10" type="number" required> )</label>'
     else:
-        location_question = '<label for="location">Specific Address/Coordinates:</label><input type="text" class="form-control" id="location" maxlength="40" name="location" required>'
+        location_question = '<label for="location">Specific Address/Coordinates: </label><input type="text" class="form-control" id="location" maxlength="40" name="location" required>'
     return render_template('maps-embed.html', checkboxes = Markup(checkboxes), report_limit = Markup(report_limit), submit = disable)
 
 @app.route('/ranks')
