817472035f7da76c6db6b47d74c1b5f68c22400f
Ramon Wang <ramonwang.sb@gmail.com>
2021-09-12 11:12:04 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,17 +1,15 @@
-$(function(){  // $(document).ready shorthand
+$(function(){ 
   $('.monster').fadeIn('slow');
 });
 
 $(document).ready(function() { 
   $(window).scroll( function(){
     
-        /* Check the location of each desired element */
         $('.fade-in').each( function(i){
             
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it in */
             if( bottom_of_window > bottom_of_object ){
                 
                 $(this).animate({'opacity':'1'},1500);
@@ -21,6 +19,7 @@ $(document).ready(function() {
         }); 
     
   });
+  $("#blue-bg").fadeIn(500)
   $("#reports").hide();
   $("#legend").hide();
   $("#legend-embed").hide();
