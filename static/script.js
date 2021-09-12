2e44b28918bb814e1c46ad80493eb87e21050c89
Ramon Wang <ramonwang.sb@gmail.com>
2021-09-12 11:15:43 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,25 +1,4 @@
-$(function(){ 
-  $('.monster').fadeIn('slow');
-});
-
 $(document).ready(function() { 
-  $(window).scroll( function(){
-    
-        $('.fade-in').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1500);
-                    
-            }
-            
-        }); 
-    
-  });
-  $("#blue-bg").fadeIn(500)
   $("#reports").hide();
   $("#legend").hide();
   $("#legend-embed").hide();
